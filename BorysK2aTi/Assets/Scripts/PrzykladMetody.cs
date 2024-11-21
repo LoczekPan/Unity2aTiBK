fc33113157a779dfb970e3a39c306b6c55c73f3a
rzygulec <152405497+rzygulec@users.noreply.github.com>
2024-11-21 12:15:06 +0100

    Added score text

---
+++ BorysK2aTi/Assets/Scripts/PrzykladMetody.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrzykladMetody : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Witaj w swiecie Unity!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
