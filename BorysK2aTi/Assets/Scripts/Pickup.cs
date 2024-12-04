f84009309c1ade52403d1eb150b9726bfbe2cff3
rzygulec <152405497+rzygulec@users.noreply.github.com>
2024-12-04 13:01:25 +0100

    Added coin pickup

--- BorysK2aTi/Assets/Scripts/Pickup.cs
+++ BorysK2aTi/Assets/Scripts/Pickup.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
@@ -7,6 +8,7 @@ using UnityEngine.SocialPlatforms.Impl;
 public class Pickup : MonoBehaviour
 {
     float score = 0;
+    [SerializeField] float speed = 1.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,9 +20,33 @@ public class Pickup : MonoBehaviour
     {
         if (other.gameObject.tag == "Coin")
         {
-            Destroy(other.gameObject);
+            StartCoroutine(MoveToPlayer(other.transform, transform, speed));
             score++;
-            Debug.Log("Twoj wynik to" + score);
+            Debug.Log("Twoj wynik to " + score);
+
+        }
+    }
+
+    IEnumerator MoveToPlayer(Transform coin, Transform player, float speed)
+    {
+        float duration = 1.5f;
+
+        float time = 0;
+
+        while (time < speed)
+        {
+            coin.position = UnityEngine.Vector3.MoveTowards(coin.position, player.position, speed * Time.deltaTime);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        Destroy(coin.gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            Destroy(collision.gameObject);
         }
     }
 
